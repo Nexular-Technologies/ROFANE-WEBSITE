c74266f9ec8f94e802e57d181957a6af5c1a3ce1
Nicholas-Henwood <nicholashenwoodo56@gmail.com>
2026-05-04 15:55:39 +0200

    Blog optimization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function HomePage() {
-  return (
-    <main>
-      <h1>Rofane Website</h1>
-      <p>Blog engine is available now.</p>
-      <p>
-        <Link href="/blog">Open public blog</Link>
-      </p>
-      <p>
-        <Link href="/admin/blog">Open admin blog</Link>
-      </p>
-    </main>
-  );
+  redirect("/index.html");
 }
